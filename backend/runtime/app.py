76612c5758bbc00c2c492a687739f5a7329d43e0
Hugo Alvarado <hugo@britecore.com>
2021-09-05 22:26:45 -0600

    Add new backend - cdk + chalice

---
+++ backend/runtime/app.py
@@ -0,0 +1,55 @@
+import os
+import boto3
+from chalice import Chalice
+
+
+app = Chalice(app_name='hiptv')
+dynamodb = boto3.resource('dynamodb')
+dynamodb_table = dynamodb.Table(os.environ.get('APP_TABLE_NAME', ''))
+
+
+
+# sources for channels:
+# http://www.reloltv.com/ Playerjs
+# ustvgo.la ..
+# http://123tv.live/
+# https://github.com/iptv-restream/iptv-channels
+#
+# @app.route('/update_keys')
+# def update_keys():
+#     import copy
+#     response = dynamodb_table.query(
+#         KeyConditionExpression=Key('pk').eq('channels')
+#     )
+#     for item in response['Items']:
+#         new_item = copy.deepcopy(item)
+#
+#         dynamodb_table.delete_item(
+#             Key={
+#                 'pk': item['pk'],
+#                 'sk': item['sk']
+#             }
+#         )
+#
+#         new_item['category'] = new_item['sk'].split('#')[1]
+#         new_item['title'] = new_item['sk'].split('#')[3]
+#
+#         print(new_item)
+#
+#         dynamodb_table.put_item(Item=new_item)
+
+
+def get_all_channels():
+    response = dynamodb_table.query(
+        KeyConditionExpression=Key('pk').eq('channels')
+    )
+    return response['Items']
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+
+@app.route('/channels', cors=True)
+def channels():
+    return get_all_channels()
